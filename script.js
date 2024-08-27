43b0609eb9c2592c3bce94e74305413bc401918b
Seyyid Yildiz <seyyid.yildiz.46@hotmail.de>
2024-08-27 15:21:47 +0200

    Coded the Backend for the Contact Form -change and set up some settings in index.html -coded the backend to send the information to my e-mail. Used web3 for this

--- script.js
+++ script.js
@@ -5,3 +5,42 @@ menu.onclick = () => {
     menu.classList.toggle('bx-x');
     nav.classList.toggle('active');
 }
+
+const form = document.getElementById('form');
+const result = document.getElementById('result');
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const formData = new FormData(form);
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+    result.innerHTML = "Einen Moment bitte..."
+
+    fetch('https://api.web3forms.com/submit', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: json
+    })
+        .then(async (response) => {
+            let json = await response.json();
+            if (response.status === 200) {
+                result.innerHTML = "Nachricht gesendet!";
+            } else {
+                console.log(response);
+                result.innerHTML = json.message;
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            result.innerHTML = "Ups! Etwas ist schiefgelaufen...";
+        })
+        .then(function() {
+            form.reset();
+            setTimeout(() => {
+                result.style.display = "none";
+            }, 3000);
+        });
+});
